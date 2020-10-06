cf88b509a2130f6253ecb00892e09ce602d43e7c
ekokratos <adithyams.adi@gmail.com>
2020-10-06 09:33:00 +0530

    Migrated to recent Flutter version, updated project structure

--- lib/login_page_4/registrationpage4.dart
+++ lib/login_page_4/registrationpage4.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'validation.dart';
+import 'package:sample_designs/utils/validation.dart';
 
 class Registration4 extends StatefulWidget {
   @override
