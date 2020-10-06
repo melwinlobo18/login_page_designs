c2c8ee2aa8001e49f94c9331592adb13465542a5
ekokratos <adithyams.adi@gmail.com>
2020-10-06 14:39:32 +0530

    Updated Gallery view

--- lib/gallery.dart
+++ lib/gallery.dart
@@ -1,64 +1,86 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-class _PageSelector extends StatelessWidget {
+List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
+List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          child: Container(
+            margin: EdgeInsets.all(5.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                child: Stack(
+                  children: <Widget>[
+                    Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(200, 0, 0, 0),
+                              Color.fromARGB(0, 0, 0, 0)
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        child: Text(
+                          'No. ${imgList.indexOf(item)} image',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ))
+    .toList();
+
+class _PageSelector extends StatefulWidget {
   const _PageSelector({this.cards});
 
   final List<Card> cards;
 
-  void _handleArrowButtonPress(BuildContext context, int delta) {
-    final TabController controller = DefaultTabController.of(context);
-    if (!controller.indexIsChanging)
-      controller
-          .animateTo((controller.index + delta).clamp(0, cards.length - 1));
-  }
+  @override
+  __PageSelectorState createState() => __PageSelectorState();
+}
 
+class __PageSelectorState extends State<_PageSelector> {
   @override
   Widget build(BuildContext context) {
-    final TabController controller = DefaultTabController.of(context);
-    final Color color = Theme.of(context).accentColor;
+    double height = MediaQuery.of(context).size.height;
     return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(top: 16.0),
-            child: Row(
-              children: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.chevron_left),
-                  color: color,
-                  onPressed: () {
-                    _handleArrowButtonPress(context, -1);
-                  },
-                  tooltip: 'Page back',
-                ),
-                TabPageSelector(controller: controller),
-                IconButton(
-                  icon: const Icon(Icons.chevron_right),
-                  color: color,
-                  onPressed: () {
-                    _handleArrowButtonPress(context, 1);
-                  },
-                  tooltip: 'Page forward',
-                ),
-              ],
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Center(
+      child: CarouselSlider(
+        options: CarouselOptions(
+            enlargeCenterPage: true,
+            enableInfiniteScroll: false,
+            height: height * 0.65,
+            initialPage: 1,
+            scrollPhysics: BouncingScrollPhysics()
+            // autoPlay: true,
             ),
-          ),
-          Flexible(
-            child: Container(
-              margin: EdgeInsets.only(top: 60),
-              height: 560,
-              width: 360,
-              child: TabBarView(
-                children: cards.map<Widget>((Card cards) {
-                  return cards;
-                }).toList(),
-              ),
-            ),
-          ),
-        ],
+        items: widget.cards.map<Widget>((Card cards) {
+          return cards;
+        }).toList(),
       ),
-    );
+    ));
   }
 }
 
@@ -67,10 +89,14 @@ class Gallery extends StatelessWidget {
 
   List cardList(BuildContext context) {
     final List<Card> cards = <Card>[
-      Cards(imgPath: 'images/Login1.jpeg', path: '/login1').build(context),
-      Cards(imgPath: 'images/Login2.png', path: '/login2').build(context),
-      Cards(imgPath: 'images/Login3.jpeg', path: '/login3').build(context),
-      Cards(imgPath: 'images/Login4.png', path: '/login4').build(context)
+      CarouselCard(imgPath: 'assets/images/Login1.jpeg', path: '/login1')
+          .build(context),
+      CarouselCard(imgPath: 'assets/images/Login2.png', path: '/login2')
+          .build(context),
+      CarouselCard(imgPath: 'assets/images/Login3.jpeg', path: '/login3')
+          .build(context),
+      CarouselCard(imgPath: 'assets/images/Login4.png', path: '/login4')
+          .build(context)
     ];
     return cards;
   }
@@ -86,41 +112,23 @@ class Gallery extends StatelessWidget {
   }
 }
 
-class Cards extends StatelessWidget {
+class CarouselCard extends StatelessWidget {
   final String imgPath;
   final String path;
-  Cards({this.imgPath, this.path});
+  CarouselCard({this.imgPath, this.path});
   @override
   Widget build(BuildContext context) {
     return Card(
-      semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      child: Column(
-        children: <Widget>[
-          Container(
-            width: 400,
-            height: 450,
-            child: Image.asset(imgPath, fit: BoxFit.cover),
-          ),
-          SizedBox(
-            height: 18,
-          ),
-          Center(
-            child: RaisedButton(
-                elevation: 5,
-                padding: EdgeInsets.all(16),
-                child: Icon(
-                  Icons.open_in_new,
-                  size: 20,
-                  color: Colors.white,
-                ),
-                color: Colors.grey[600],
-                onPressed: () {
-                  Navigator.pushNamed(context, path);
-                },
-                shape: CircleBorder()),
-          )
-        ],
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, path);
+        },
+        child: Container(
+          // width: 400,
+          // height: 450,
+          child: Image.asset(imgPath, fit: BoxFit.fill),
+        ),
       ),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
